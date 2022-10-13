c560d4648ef982805e14189aef3ba1711924bf80
Olawale Abaniwonda <olawalebash2017@gmail.com>
2022-10-13 15:00:52 +0100

    Update BrakeDust.cs

--- Platformer.Engine/Gameplay/Entities/BrakeDust.cs
+++ Platformer.Engine/Gameplay/Entities/BrakeDust.cs
@@ -1,5 +1,5 @@
 ﻿/*
- *  Copyright (c) 2016 Rob Harwood <rob@codemlia.com>
+ *  Copyright (c) 2022 Olawale Abaniwonda <olawalebash2017@gmail.com>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software
  *  and associated documentation files (the "Software"), to deal in the Software without
